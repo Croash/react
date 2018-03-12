e86c71a06c187fb5614c338f8e5505f05e75d56b
Unknown <1327914853@qq.com>
2018-03-13 00:31:56 +0800

    试用react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import Input from './input'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { Redirect } from 'react-router'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import './App.css'
+
+class Test extends Component {
+  render() {
+    return (
+      <div style = {{ width:'1000px',height:'1000px',backgroundColor:'red' }}>
+        sgsg
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <Input/>
+          <BrowserRouter basename = {'/'}>
+            <div>
+              <Route path = {'/input'}
+                component = { Input }
+                forceRefresh = { false }
+              >
+              </Route>
+              <Route path = {'/test'}
+                component = { Test }
+                forceRefresh = { false }
+              >
+              </Route>
+            </div>
+          </BrowserRouter>
         </div>
       </MuiThemeProvider>
     )
