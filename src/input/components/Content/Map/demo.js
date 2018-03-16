efb1856387cfd904b2a5ab979bdddd76b024cc18
邵泽林 <shaozelin@etstone.cn>
2018-03-16 17:04:24 +0800

    edit markdown layout

--- src/input/components/Content/Map/demo.js
+++ src/input/components/Content/Map/demo.js
@@ -4,6 +4,7 @@ const demo = `
 算是一个对amapui的封装，重构过一次。
 之所以写这么个小项目，也是开始时候项目中用到了这个。后来比较闲的时候，把整个amapui看了一遍，然后把react-amap的组件构造方法搬运了过来= = 
 还算好用，不过在componentWillUnmount时，还有比较多问题，后面慢慢解决吧。
+
 下面这个是一个点击对应省，就会将该省进行染色的demo，粗略的随便写了一点，大家看看效果吧。
 
 地址就在这里了: [react-amapui-wrapper](https://github.com/Croash/react-amapui-wrapper) 可自定义的AMapUI组件
